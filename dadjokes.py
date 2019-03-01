a96bc6526a72bde809750b5c62e0c8bd95f36ec2
Elizabeth Wehner <ewehner@gmail.com>
2019-03-01 17:30:50 -0600

    Now assembles a very random sentence

--- dadjokes.py
+++ dadjokes.py
@@ -8,22 +8,38 @@ class MainHandler(tornado.web.RequestHandler):
         joke = get_random_joke()
 
         # output to console
-        print(joke.text)
+        print(joke)
 
         # output to webpage
-        self.write(joke.text)
+        self.write(joke)
 
 
 class BuildAJokeHandler(tornado.web.RequestHandler):
     def get(self):
-        joke = get_random_joke()
-        words = joke.text.split()
-        print(words)
+        new_joke = ''
+        step = 0
+
+        while (True):
+
+            joke = get_random_joke().lower()
+            joke_words = joke.split()
+            print(joke_words)
+
+            if step < len(joke_words):
+                new_joke += joke_words[step] + ' '
+                step += 1
+            else:
+                break
+
+
+        print(new_joke)
+        self.write(new_joke)
 
 
 def get_random_joke():
     headers = {'user-agent': 'ewehner', 'Accept': 'text/plain'}
-    return requests.get("https://icanhazdadjoke.com/", headers=headers)
+    r = requests.get("https://icanhazdadjoke.com/", headers=headers)
+    return r.text
 
 
 
@@ -31,7 +47,7 @@ if __name__ == "__main__":
     static_url_prefix = "/static/"
     dad_jokes = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/markov", BuildAJokeHandler)
+        (r"/markov.*", BuildAJokeHandler)
     ], debug=True)
 
     logging.warning('DadJokes is now listening on port 8888...')
