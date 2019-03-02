c6786008fb1934b07abdc55d8bfcae4e945a907f
Elizabeth Wehner <ewehner@gmail.com>
2019-03-01 18:47:08 -0600

    list now works

--- dadjokes.py
+++ dadjokes.py
@@ -2,6 +2,9 @@ import tornado.web
 import tornado.ioloop
 import requests
 import logging
+import json
+
+from pprint import pprint
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -36,18 +39,35 @@ class BuildAJokeHandler(tornado.web.RequestHandler):
         self.write(new_joke)
 
 
+class ListHandler(tornado.web.RequestHandler):
+    def get(self):
+        jokes = get_list_of_jokes()
+        self.render('list.html', title="Woo Dad Jokes!", items=jokes)
+
+
 def get_random_joke():
     headers = {'user-agent': 'ewehner', 'Accept': 'text/plain'}
     r = requests.get("https://icanhazdadjoke.com/", headers=headers)
     return r.text
 
+def get_list_of_jokes():
+    headers = {'user-agent': 'ewehner', 'Accept': 'application/json'}
+    payload = {"limit": "30", "page": "1"}
+    r = requests.get("https://icanhazdadjoke.com/search", headers=headers, params=payload)
+
+    jokes = r.json()['results']
+
+    jokes_only = list(map(lambda x: x['joke'], jokes))
+
+    return jokes_only
 
 
 if __name__ == "__main__":
     static_url_prefix = "/static/"
     dad_jokes = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/markov.*", BuildAJokeHandler)
+        (r"/markov.*", BuildAJokeHandler),
+        (r"/list.*", ListHandler)
     ], debug=True)
 
     logging.warning('DadJokes is now listening on port 8888...')
